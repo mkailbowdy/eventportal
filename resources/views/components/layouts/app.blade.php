3dfa646f0fab409bd13f3e04ec2bfd829cd4d4b1
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-05-08 08:45:08 +0900

    eventportal

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,13 +7,13 @@
     <title>{{ $title ?? 'Page Title' }}</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
 
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-    <body>
-    <x-header/>
-        {{ $slot }}
-    </body>
+<body class="h-full">
+<x-header/>
+{{ $slot }}
+</body>
 </html>
--- resources/views/livewire/events/event-show.blade.php
+++ resources/views/livewire/events/event-show.blade.php
@@ -1,3 +1,22 @@
-<div>
-    {{$this->event->title}}
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+<div class="min-h-full">
+    <div class="py-10">
+        <header>
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Dashboard</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <!-- Your content -->
+            </div>
+        </main>
+    </div>
 </div>
