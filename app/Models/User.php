ee10f4a005b98c56748d0fdefbda0580a1636593
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-06-02 12:16:26 +0900

    predeployment1

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
     use HasRoles;
 
+    protected $guard_name = 'api';
     /**
      * The attributes that are mass assignable.
      *
