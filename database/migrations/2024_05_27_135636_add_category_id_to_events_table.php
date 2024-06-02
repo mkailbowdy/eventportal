baddef14d2e1fa5afe17a57ec2698821af5116a6
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-06-02 21:36:12 +0900

    predeployment

--- database/migrations/2024_05_27_135636_add_category_id_to_events_table.php
+++ database/migrations/2024_05_27_135636_add_category_id_to_events_table.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     /**
@@ -27,11 +25,11 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('events', function (Blueprint $table) {
-            //
-        });
-        Schema::table('groups', function (Blueprint $table) {
-            //
-        });
+//        Schema::table('events', function (Blueprint $table) {
+//            //
+//        });
+//        Schema::table('groups', function (Blueprint $table) {
+//            //
+//        });
     }
 };
