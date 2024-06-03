c2e2033006c4bf8e7d7e9cfa16784400677b6c6f
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-06-03 10:59:38 +0900

    foreignId columns

--- database/migrations/2024_05_27_135636_add_category_id_to_events_table.php
+++ database/migrations/2024_05_27_135636_add_category_id_to_events_table.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     /**
@@ -8,15 +10,15 @@ return new class extends Migration {
      */
     public function up(): void
     {
-//        Schema::table('events', function (Blueprint $table) {
-////            $table->foreignId('category_id')->after('id');
-////            $table->foreignId('group_id')->constrained()->onDelete('cascade');
-//
-//        });
+        Schema::table('events', function (Blueprint $table) {
+            $table->foreignId('category_id')->after('id');
+            $table->foreignId('group_id')->constrained()->onDelete('cascade');
 
-//        Schema::table('groups', function (Blueprint $table) {
-//            $table->foreignId('category_id')->constrained()->onDelete('cascade');
-//        });
+        });
+
+        Schema::table('groups', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+        });
 
     }
 
@@ -25,11 +27,12 @@ return new class extends Migration {
      */
     public function down(): void
     {
-//        Schema::table('events', function (Blueprint $table) {
-//            //
-//        });
-//        Schema::table('groups', function (Blueprint $table) {
-//            //
-//        });
+        Schema::table('events', function (Blueprint $table) {
+            $table->foreignId('category_id')->after('id');
+            $table->foreignId('group_id')->constrained()->onDelete('cascade');
+        });
+        Schema::table('groups', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+        });
     }
 };
