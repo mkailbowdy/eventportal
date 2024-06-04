2936db6b0dac71f4cc8472334eaa4f9ef2905f2d
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-06-04 23:54:12 +0900

    fix Dashboard and navigation

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -67,16 +67,22 @@ new class extends Component {
                                 wire:navigate>
                         {{ __('Groups') }}
                     </x-nav-link>
-
-                    @role('member')
-                    <x-nav-link :href="route('groups.create')" :active="request()->routeIs('groups.create')"
-                                wire:navigate>
-                        {{ __('Create a Group') }}
-                    </x-nav-link>
-                    @endrole
                 </div>
             </div>
             <div class="flex items-center">
+                @role('member')
+                <div class="flex-shrink-0 mr-1">
+                    <a wire:navigate
+                       href="{{route('groups.create')}}"
+                       class="relative inline-flex items-center gap-x-1.5 rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500">
+                        <svg class="-ml-0.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path
+                                d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"/>
+                        </svg>
+                        Create Group
+                    </a>
+                </div>
+                @endrole
                 <div class="flex-shrink-0">
                     <a wire:navigate
                        href="{{route('events.create')}}"
@@ -89,6 +95,7 @@ new class extends Component {
                     </a>
                 </div>
                 <div class="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
+
                     <button type="button"
                             class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span class="absolute -inset-1.5"></span>
--- resources/views/livewire/user-dashboard.blade.php
+++ resources/views/livewire/user-dashboard.blade.php
@@ -1,72 +1,44 @@
 <div class="px-4 sm:px-6 lg:px-8">
     <div class="sm:flex sm:items-center">
-        <div class="sm:flex-auto">
-            <h1 class="text-base font-semibold leading-6 text-gray-900">Your Events</h1>
-            <p class="mt-2 text-sm text-gray-700">All the events you're going to in one spot!</p>
-        </div>
-        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-            <button type="button"
-                    class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Add user
-            </button>
+        <div class="py-3">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Your Events</h2>
         </div>
     </div>
-    <div class="mt-8 flow-root">
-        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                <table class="min-w-full divide-y divide-gray-300">
-                    <thead>
-                    <tr>
-                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                            Name
-                        </th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Date</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Participants
-                        </th>
-                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                            <span class="sr-only">Edit</span>
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody class="divide-y divide-gray-200 bg-white">
 
-                    @foreach($events as $event)
-                        <tr>
-                            <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                                <div class="flex items-center">
-                                    <div class="h-32 w-32 flex-shrink-0">
-                                        <img class="h-32 w-32 rounded-md object-cover"
-                                             src="{{asset('storage/' . $event->photo_path)}}"
-                                             alt="">
-                                    </div>
-                                    <div class="ml-4">
-                                        <div class="font-medium text-gray-900">{{$event->title}}</div>
-                                        <div class="mt-1 text-gray-500">{{$event->group->name}}</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                                <div class="text-gray-900">{{$event->event_date->format('D, M jS')}}</div>
-                                <div class="mt-1 text-gray-500">{{$event->start_time->format('g:i A')}}
-                                    to {{$event->end_time->format('g:i A')}}</div>
-                            </td>
-                            <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                                <div
-                                    class="text-gray-900">{{$event->users()->wherePivot('participation_status', 1)->count()}}
-                                    / {{$event->max_participants}}</div>
-                            </td>
-
-                            <td class="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                                <a href="{{ route('events.show', ['event' => $event->id]) }}"
-                                   class="text-indigo-600 hover:text-indigo-900">View</a>
-                            </td>
-                        </tr>
-                    @endforeach
-
-                    <!-- More people... -->
-                    </tbody>
-                </table>
+    @if($events->isEmpty())
+        <div class="pb-20">
+            <div class="p-2 bg-gray-500 text-white rounded">Go join some events :) Events will show here when you mark
+                'Going' on an event
             </div>
         </div>
+    @endif
+
+    <div class="pb-20">
+        @foreach($events as $event)
+            <div class="flex flex-row py-3">
+                <div class="flex-shrink-0">
+                    <a href="{{route('events.show', ['event' => $event->id])}}">
+                        <img class="h-24 w-24 rounded-md object-cover"
+                             src="{{asset('storage/' . $event->photo_path)}}"
+                             alt="">
+                    </a>
+                </div>
+                <div class="flex-col ml-6">
+                    <div><a
+                            href="{{route('events.show', ['event' => $event->id])}}"><strong>{{$event->title}}</strong></a>
+                    </div>
+                    <div class="text-gray-500">{{$event->group->name}}</div>
+                    <div>
+                        <div>{{$event->event_date->format('D, M jS')}}
+                            ・{{$event->start_time->format('H:i')}}
+                            to {{$event->end_time->format('H:i')}}</div>
+                    </div>
+
+                    <div>Going: {{$event->users()->wherePivot('participation_status', 1)->count()}}
+                        / {{$event->max_participants}}</div>
+                </div>
+            </div>
+        @endforeach
     </div>
+
 </div>
