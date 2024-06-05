33697f0616d01dbd64f29ffcdacf2a0a2cf15059
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-06-05 15:08:05 +0900

    add search and filters to groupindex

--- app/Livewire/GroupIndex.php
+++ app/Livewire/GroupIndex.php
@@ -2,20 +2,70 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use App\Models\Group;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Stevebauman\Location\Facades\Location;
+use Transliterator;
 
 #[Title('Groups')]
 class GroupIndex extends Component
 {
     use WithPagination;
 
+    public Collection $categories;
+    public int $searchCategory = 0;
+    public string $searchQuery = '';
+    public string $searchPrefecture = '';
+
+    public function mount(): void
+    {
+        $this->categories = Category::pluck('name', 'id');
+        $ip = request()->ip();
+        $currentLocation = Location::get($ip);
+
+        if (!$currentLocation || $currentLocation->countryName !== 'Japan') {
+            $this->searchPrefecture = 'Tokyo';
+        } else {
+            $transliterator = Transliterator::create('NFD; [:Nonspacing Mark:] Remove; NFC;');
+            $this->searchPrefecture = $transliterator->transliterate($currentLocation->regionName);
+        }
+    }
+
+    // updating() is a lifecycle hook. Any time the component is updated, the pagination pages is reset
+    public function updating($key): void
+    {
+        if ($key === 'searchQuery' || $key === 'searchCategory' || $key === 'searchPrefecture') {
+            $this->resetPage();
+        }
+    }
+
     public function render()
     {
+        $groups = Group::with(['users'])
+            ->orderBy('name', 'ASC')
+            ->when($this->searchQuery !== '',
+                fn(Builder $query) => $query->where('name', 'like', '%'.$this->searchQuery.'%'))
+            ->when($this->searchCategory > 0, fn(Builder $query) => $query->where('category_id', $this->searchCategory))
+            ->when($this->searchPrefecture !== '',
+                fn(Builder $query) => $query->where('prefecture', 'like', '%'.$this->searchPrefecture.'%'))
+//            ->whereHas('group', function ($query) {
+//                $query->where('prefecture', $this->searchPrefecture);
+//            })
+            ->paginate(8);
+
+
+        // We're passing to the view a variable called $events which contains an Event model that includes all records.
         return view('livewire.groups.group-index', [
-            'groups' => Group::orderBy('created_at', 'desc')->paginate(10),
+            'groups' => $groups,
         ]);
+
+//        return view('livewire.groups.group-index', [
+//            'groups' => Group::orderBy('created_at', 'desc')->paginate(10),
+//        ]);
     }
 }
--- resources/views/livewire/groups/group-index.blade.php
+++ resources/views/livewire/groups/group-index.blade.php
@@ -1,4 +1,4 @@
-<div class="mx-auto max-w-7xl sm:px-6 lg:px-8 py-5">
+<div class="mx-auto max-w-7xl px-3 sm:px-6 lg:px-8 py-5">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('All Groups') }}
@@ -19,35 +19,59 @@
         </div>
     </div>
 
-    <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        @foreach($groups as $group)
-            <li wire:key="{{$group->id}}"
-                class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
-                <div class="flex flex-1 flex-col p-8">
-                    <a href="/groups/{{$group->id}}">
-                        @if ($group->photo_path)
-                            <img src="{{ asset('storage/' . $group->photo_path) }}" alt="group Photo"
-                                 class="h-full w-full object-cover object-center lg:h-full lg:w-full">
-                        @else
-                            <img src="{{ asset('storage/' . 'photos/placeholder_image.png' ) }}"
-                                 alt="Group Photo" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
-                        @endif
-                    </a>
-                    <h3 class="mt-2 font-medium text-gray-900">{{$group->name}}</h3>
-                    <small>Organized by
-                        @if ($group->users()->wherePivot('role', 'organizer')->first())
-                            {{$group->users()->wherePivot('role', 'organizer')->first()->name}}
-                        @else
-                            {{'No One'}}
-                        @endif
-                    </small>
-                    <dl class="mt-2 flex flex-grow flex-col justify-between">
-                        <dt class="sr-only">Description</dt>
-                        <dd class="text-sm text-gray-500">{{ $group->description }}</dd>
-                    </dl>
-                </div>
-            </li>
-        @endforeach
-    </ul>
-    {{$groups->links()}}
+    <div class="py-4">
+        <div class="space-y-4 sm:space-y-0 sm:space-x-4 sm:flex sm:items-center">
+            <input wire:model.live="searchQuery" type="search" placeholder="Search by Name"/>
+            <select wire:model.live="searchPrefecture" name="prefecture"
+                    class="block w-full py-2 pl-3 pr-10 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <option value="Japan">Choose a prefecture</option>
+                @foreach(App\Enums\Prefecture::cases() as $prefecture)
+                    <option value="{{$prefecture->value}}">{{$prefecture->label()}}</option>
+                @endforeach
+            </select>
+            <select wire:model.live="searchCategory" name="category"
+                    class="block w-full py-2 pl-3 pr-10 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <option value="0">Choose a category</option>
+                @foreach($categories as $id => $category)
+                    <option value="{{$id}}">{{$category}}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+
+    <div>
+        <ul role="list" class="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            @foreach($groups as $group)
+                <li wire:key="{{$group->id}}"
+                    class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
+                    <div class="flex flex-1 flex-col p-8">
+                        <a href="/groups/{{$group->id}}">
+                            @if ($group->photo_path)
+                                <img src="{{ asset('storage/' . $group->photo_path) }}" alt="group Photo"
+                                     class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                            @else
+                                <img src="{{ asset('storage/' . 'photos/placeholder_image.png' ) }}"
+                                     alt="Group Photo"
+                                     class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                            @endif
+                        </a>
+                        <h3 class="mt-2 font-medium text-gray-900">{{$group->name}}</h3>
+                        <small>Organized by
+                            @if ($group->users()->wherePivot('role', 'organizer')->first())
+                                {{$group->users()->wherePivot('role', 'organizer')->first()->name}}
+                            @else
+                                {{'No One'}}
+                            @endif
+                        </small>
+                        <dl class="mt-2 flex flex-grow flex-col justify-between">
+                            <dt class="sr-only">Description</dt>
+                            <dd class="text-sm text-gray-500 line-clamp-3">{{ $group->description }}</dd>
+                        </dl>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+        {{$groups->links()}}
+    </div>
+
 </div>
