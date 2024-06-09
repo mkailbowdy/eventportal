6bbe3e311b4ab1735a6c1bc7286b76e3595cd38f
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-06-09 20:08:45 +0900

    fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,24 +17,22 @@
 @guest
     <x-header/>
 @endguest
-<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-    @auth
-        <livewire:layout.navigation/>
-    @endauth
-    <!-- Page Heading -->
-    @if (isset($header))
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-    @endif
+@auth
+    <livewire:layout.navigation/>
+@endauth
+<!-- Page Heading -->
+@if (isset($header))
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
+@endif
 
-    <!-- Page Content -->
-    <main>
-        {{ $slot }}
-    </main>
-</div>
+<!-- Page Content -->
+<main>
+    {{ $slot }}
+</main>
 <div>
     <x-layouts.footer/>
 </div>
